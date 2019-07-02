4eb17a1444e2195ab503a31d48b76309f5434119
Mark van Holsteijn <markvanholsteijn@binx.io>
2019-07-02 13:27:47 +0200

    added ARN diff

--- src/ssm_parameter_name.py
+++ src/ssm_parameter_name.py
@@ -9,7 +9,11 @@ def to_arn(region, account_id, name):
     returns the arn of an SSM parameter with specified, region, account_id and name. If the name
     starts with a '/', it does not show up in the Arn as AWS seems to store it that way.
     """
-    return 'arn:aws:ssm:%s:%s:parameter/%s' % (region, account_id, name if name[0] != '/' else name[1:])
+    return "arn:aws:ssm:%s:%s:parameter/%s" % (
+        region,
+        account_id,
+        name if name[0] != "/" else name[1:],
+    )
 
 
 def from_arn(arn):
@@ -17,9 +21,20 @@ def from_arn(arn):
     Returns the parameter name from a parameter name Arn, or None if not found.
 
     """
-    m = re.match(arn_regexp, arn)
-    name = m.group('name') if m else None
-    return name if not name or '/' not in name or name[0] == '/' else '/{}'.format(name)
+    m = arn_regexp.match(arn)
+    name = m.group("name") if m else None
+    return name if not name or "/" not in name or name[0] == "/" else "/{}".format(name)
 
 
-arn_regexp = re.compile(r'arn:aws:ssm:(?P<region>[^:]*):(?P<account>[^:]*):parameter/(?P<name>.*)')
+def equals(arn1, arn2):
+    m1 = arn_regexp.match(arn1) if arn1 else None
+    m2 = arn_regexp.match(arn2) if arn2 else None
+    if not m1 or not m2:
+        return False
+
+    return m1.groupdict() == m2.groupdict()
+
+
+arn_regexp = re.compile(
+    r"arn:aws:ssm:(?P<region>[^:]*):(?P<account>[^:]*):parameter/+(?P<name>.*)"
+)
--- tests/test_ssm_parameter_name.py
+++ tests/test_ssm_parameter_name.py
@@ -2,12 +2,13 @@ import ssm_parameter_name
 
 
 def test_arn_to_name():
-    region = 'eu-central-1'
-    account = '111111111114'
+    region = "eu-central-1"
+    account = "111111111114"
     test_set = {
-        'issue-25': 'issue-25',
-        'demo/issue-25': '/demo/issue-25',
-        '/demo/issue-25': '/demo/issue-25'
+        "issue-25": "issue-25",
+        "/issue-25": "issue-25",
+        "demo/issue-25": "/demo/issue-25",
+        "/demo/issue-25": "/demo/issue-25",
     }
     for name, expect in test_set.items():
         arn = ssm_parameter_name.to_arn(region, account, name)
@@ -16,11 +17,33 @@ def test_arn_to_name():
 
 def test_name_from_arn():
     test_set = {
-        'arn:aws:ssm:eu-central-1:111111111114:parameter/issue-25': 'issue-25',
-        'arn:aws:ssm:eu-central-1:111111111114:parameter/demo/issue-25': '/demo/issue-25',
-        'arn:aws:ssm:eu-central-1:111111111114:parameter//demo/issue-25': '/demo/issue-25',
-        'arn:aws:ssm:eu-central-1:111111111114:document/SSM:RunShell': None
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/issue-25": "issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter//issue-25": "issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/demo/issue-25": "/demo/issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter//demo/issue-25": "/demo/issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:document/SSM:RunShell": None,
     }
     for arn, expect in test_set.items():
         name = ssm_parameter_name.from_arn(arn)
         assert expect == name
+
+
+def test_equals():
+    test_set_equals = {
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/issue-25": "arn:aws:ssm:eu-central-1:111111111114:parameter//issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/demo/issue-25": "arn:aws:ssm:eu-central-1:111111111114:parameter/demo/issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/demo/issue-25": "arn:aws:ssm:eu-central-1:111111111114:parameter//demo/issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/demo/issue-25": "arn:aws:ssm:eu-central-1:111111111114:parameter//demo/issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/////demo/issue-25": "arn:aws:ssm:eu-central-1:111111111114:parameter//demo/issue-25",
+    }
+    for arn1, arn2 in test_set_equals.items():
+        assert ssm_parameter_name.equals(arn1, arn2), f"{arn1} != {arn2}"
+
+    test_set_not_equals = {
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/issue-25": "issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/issue-25": "arn:aws:ssm:eu-west-1:111111111114:parameter/issue-25",
+        "arn:aws:ssm:eu-central-1:111111111114:parameter/issue-25": "arn:aws:ssm:eu-central-1:132312323134:parameter/issue-25",
+        None: None,
+    }
+    for arn1, arn2 in test_set_not_equals.items():
+        assert not ssm_parameter_name.equals(arn1, arn2), f"{arn1} == {arn2}"
